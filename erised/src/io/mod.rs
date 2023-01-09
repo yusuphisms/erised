56add42d33ae2ee07a909911457529de922c062e
Yusuph Mkangara <yusuph.mka@outlook.com>
2023-01-09 01:28:44 -0800

    Basic file streamer

--- erised/src/io/mod.rs
+++ erised/src/io/mod.rs
@@ -1,23 +1,42 @@
-// We are going to simplify the problem to start.
-//
-//
-// What if the input is entirely in memory and only a single `set`?
-//
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
-use std::io::BufRead;
+/// What if the input is from a file?
+/// New line delimited
+pub(crate) fn from_file() {
+    let file_handle = File::open("/home/mkuki/stream/erised/erised/src/io/testfile").unwrap();
+    // In the real deal, we'd have to make many chunky reads
+    // While we can't store everything in memory, striking a balance between many
+    // network calls and memory is worth considering. BufReader and similar implementations could
+    // help here.
+    let mut buf_reader = BufReader::new(file_handle);
+    // how is it already 30 minutes?!?!?!
+    let mut new_buff = Vec::new();
+    let result = buf_reader.read_until(b'\n', &mut new_buff).unwrap();
+    println!("{:?}", String::from_utf8(new_buff.clone()));
+    while !buf_reader.buffer().is_empty() {
+        buf_reader.read_until(b'\n', &mut new_buff).unwrap();
+    }
+    println!("{:?}", String::from_utf8(new_buff));
+}
 
+/// We are going to simplify the problem to start.
+/// What if the input is entirely in memory and only a single `set`?
 pub(crate) fn only_in_memory() {
     // first task is stream data in chunks
-    let mut testing = "This, will, be, my test; bytes,".as_bytes();
+    let mut testing = "This, will\n be, my test; bytes,".as_bytes();
 
     // how is it already 30 minutes?!?!?!
     let mut new_buff = Vec::new();
-    let result = testing.read_until(b',', &mut new_buff).unwrap();
-    testing.read_until(b',', &mut new_buff).unwrap();
+    let result = testing.read_until(b'\n', &mut new_buff).unwrap();
+    println!("{:?}", String::from_utf8(new_buff.clone()));
+    //testing.read_until(b',', &mut new_buff).unwrap();
     // technically, chunks would be a really nice answer to this but in the real deal, I'll
     // probably have to find a delimiter I think
     while !testing.is_empty() {
-        testing.read_until(b',', &mut new_buff).unwrap();
+        testing.read_until(b'\n', &mut new_buff).unwrap();
     }
     println!("{:?}", String::from_utf8(new_buff));
 }
--- erised/src/main.rs
+++ erised/src/main.rs
@@ -1,6 +1,7 @@
 mod io;
-use io::only_in_memory;
+use io::{from_file, only_in_memory};
 
 fn main() {
     only_in_memory();
+    from_file();
 }
